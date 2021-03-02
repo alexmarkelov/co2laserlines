0595190eb3a3308eb26064d4535d1fad02743997
alexmarkelov <markellexa@mail.ru>
2021-03-02 15:23:27 +0300

    Calculation of wavelength is normal working Constants are corrected according Witteman book

--- co2mollines.py
+++ co2mollines.py
@@ -6,34 +6,49 @@ import molconst12c16o2
 import numpy as np
 
 
+CO210MKM = (960.9586, 0.77733033, -3.047548082e-3, -4.95971792e-7,-1.8082522e-8, 6.24e-13, -1.74e-13)
+CO29MKM = (1.0637346e3, 0.7776237, -3.340875873e-3, -5.8038975e-7, 2.4126352e-8, 7.5e-13, -2.16e-13)
+
+
+
 class CO2LaserLine():
 
     
-    def __init__(self,jnum,branch,central_wavelength):
+    MKM10 = molconst12c16o2.CO210MKM
+    MKM9 = molconst12c16o2.CO29MKM
+    
+    def __init__(self, jnum, branch, central_wavelength):
         self.jnum = jnum
         self.branch = branch
         self.cen_wav = central_wavelength
-        self.wavelength = getwavelength()
-        pass
+        self.wavelength = 0
+        self.getwavelength()
     
     def getwavelength(self):
-        if central_wavelength == 10:
-            if self.branch == P:
-                return wavelengthcalc()
-            elif():
-                return wavelengthcalc()
+        if self.cen_wav == 10:
+            if self.branch == "P":
+                self.wavelength = self.wavelengthcalc(-self.jnum, CO2LaserLine.MKM10)
+                pass
+            elif self.branch == "R":
+                self.wavelength = self.wavelengthcalc(self.jnum+1, CO2LaserLine.MKM10)
+                pass
         elif():
-            if self.branch == P:
-                return wavelengthcalc()
-            elif():
-                return wavelenghtcalc()
+            if self.branch == "P":
+                self.wavelength = self.wavelengthcalc(-self.jnum, CO2LaserLine.MKM9)
+                pass
+            elif self.branch == "R":
+                self.wavelength = self.wavelenghtcalc(self.jnum+1, CO2LaserLine.MKM9)
+                pass
+        
     
-    def wavelengthcalc():
-        return 1
+    def wavelengthcalc(self, m, constants):
+        wavelength = 0
+        for member in enumerate(constants):
+            wavelength = wavelength + m**member[0]*member[1]
+        return wavelength
 
 
-
-
-print('All is OK')
+line10P20 = CO2LaserLine(20, "P", 10)
+print(line10P20.wavelength)
  
  
