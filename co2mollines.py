8bb9c9fb4ad0c135a4e093add1a135c6f42a108b
alexmarkelov <markellexa@mail.ru>
2021-03-02 10:37:30 +0300

    - Create class CO2LaserLines to calculate wavelength of laser transition (jnum(even number from 2 to 60),     Branch(P or R), central_wavelength(9 or 10) mkm ) - Create file molconst12c16o2 with constants of co2 molecule transition

--- co2mollines.py
+++ co2mollines.py
@@ -1,1 +1,39 @@
-# Prpogramm to get wavelength of emission lines of CO2 molecule
+# Prpogramm to calculate wavelength of emission lines
+# of CO2 molecule[cm-1] in transition 10mkm, 9mkm
+
+
+import molconst12c16o2
+import numpy as np
+
+
+class CO2LaserLine():
+
+    
+    def __init__(self,jnum,branch,central_wavelength):
+        self.jnum = jnum
+        self.branch = branch
+        self.cen_wav = central_wavelength
+        self.wavelength = getwavelength()
+        pass
+    
+    def getwavelength(self):
+        if central_wavelength == 10:
+            if self.branch == P:
+                return wavelengthcalc()
+            elif():
+                return wavelengthcalc()
+        elif():
+            if self.branch == P:
+                return wavelengthcalc()
+            elif():
+                return wavelenghtcalc()
+    
+    def wavelengthcalc():
+        return 1
+
+
+
+
+print('All is OK')
+ 
+ 
