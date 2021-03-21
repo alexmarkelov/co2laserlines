72d4fdd465216a05102cc8a1d2ecdcd46913bc20
alexmarkelov <markellexa@mail.ru>
2021-03-21 16:44:10 +0300

    Some litle changes.

--- co2mollines.py
+++ co2mollines.py
@@ -8,7 +8,7 @@ import numpy as np
 
 class CO2LaserLine():
 
-    
+    C = 299792458
     MKM10 = molconst12c16o2.CO210MKM
     MKM9 = molconst12c16o2.CO29MKM
     
@@ -22,35 +22,43 @@ class CO2LaserLine():
     def getwavelength(self):
         if self.cen_wav == 10:
             if self.branch == "P":
-                self.wavelength = self.wavelengthcalc(-self.jnum, CO2LaserLine.MKM10)
+                self.wavelength = self.wavelengthcalc(-self.jnum,
+                                                      CO2LaserLine.MKM10)
                 pass
             elif self.branch == "R":
-                self.wavelength = self.wavelengthcalc(self.jnum+1, CO2LaserLine.MKM10)
+                self.wavelength = self.wavelengthcalc(self.jnum+1,
+                                                      CO2LaserLine.MKM10)
                 pass
         elif self.cen_wav == 9:
             if self.branch == "P":
-                self.wavelength = self.wavelengthcalc(-self.jnum, CO2LaserLine.MKM9)
+                self.wavelength = self.wavelengthcalc(-self.jnum,
+                                                      CO2LaserLine.MKM9)
                 pass
             elif self.branch == "R":
-                self.wavelength = self.wavelengthcalc(self.jnum+1, CO2LaserLine.MKM9)
+                self.wavelength = self.wavelengthcalc(self.jnum+1,
+                                                      CO2LaserLine.MKM9)
                 pass
         
     
     def wavelengthcalc(self, m, constants):
         wavelength = 0
         for member in enumerate(constants):
-            wavelength = wavelength + m**member[0]*member[1]
+            wavelength = wavelength + member[1]*m**member[0]
         return wavelength
 
-
+    def rcmtomkm(self):
+        self.wavelength = 10000.0/self.wavelength
+     
 def printalllines():
     branches = ("P", "R")
     centers = (10, 9)
-    for branch in branches:
-        for center in centers:
+    for center in centers:
+        for branch in branches:
             for line in range(1,25):
                 A = CO2LaserLine(2*line, branch, center)
-                print(A.cen_wav, A.branch, A.jnum,' wavelength[cm-1] - ', A.wavelength)
+                A.rcmtomkm()
+                print(A.cen_wav, A.branch, A.jnum,' wavelength[mkm] - ',
+                      A.wavelength)
         
 
 if __name__ == '__main__':
